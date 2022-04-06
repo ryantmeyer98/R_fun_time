549f29758d0d034bca43ea129ea12333de6fbb6c
Ryan Meyer <RyanMeyer@MacBook-Pro.local>
2022-04-06 13:04:27 -0500

    Added some stuff to ecoplates and started index.

---
+++ scripts/index start.R
@@ -0,0 +1,11 @@
+# Notes on xx from 2022/4/5 
+# what we went over
+# - reading in data
+# - renaming columns
+# - wide to long format
+# - pasting columns together to create a new column
+# - using case when to change the names of values within a dataset
+# - can use tolower within mutate to change case to upper or lower case
+# - use group by to do math by group 
+# - do math between cells not just within columns
+# - use summarize to make summaries
